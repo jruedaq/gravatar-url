f3c6b08529cb574d330d0a5e1f469b1ab8afd292
Juan David Rueda Quiroga <jrueda@oneago.com>
2020-09-25 10:27:03 -0500

    Add example for use

---
+++ examples/LibraryUse.php
@@ -0,0 +1,24 @@
+<?php
+require '../vendor/autoload.php';
+
+use jruedaq\GravatarUrl\GetGravatar;
+
+$name = $_POST["user"] ?? "Example User";
+$email = $_POST["email"] ?? "example@example.com";
+
+$imageUrl = GetGravatar::getUrl($name, $email, 230);
+?>
+<img src="<?php echo $imageUrl; ?>" alt="profile image">
+
+<form method="POST">
+    <br>
+    <label>Input your name
+        <input type="text" name="user" value="<?php echo $name; ?>">
+    </label>
+    <br>
+    <label>Input your email
+        <input type="email" name="email" value="<?php echo $email; ?>">
+    </label>
+    <br>
+    <input type="submit" value="Test">
+</form>
